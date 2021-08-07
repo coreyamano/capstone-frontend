af83aeb04c3fe434d834e83e1fbf30538c3b08c0
Corey <corey.amano@gmail.com>
2021-08-07 15:38:08 -0700

    add signup, login, logout routes and views

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Products from "../views/Products.vue";
-
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -20,7 +22,21 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
-
+  {
+    path: "/signup",
+    name: "signup",
+    component: Signup,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    component: Logout,
+  },
   {
     path: "/products",
     name: "Products",
